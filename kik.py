91811e1ec5f9c1d035ab7a070988ee0e6b00321a
thanakron <ningarriymm1@gmail.com>
2026-09-12 22:30:29 +0700

    Update kik.py

--- kik.py
+++ kik.py
@@ -101,8 +101,6 @@ HTML_TEMPLATE = """
 <html lang="th">
 
 <head>
-
-    <meta name="google-site-verification" content="weGo8IJjwUhkBdvIpgRURMztU0sJmoERgh6oD2y6-r8"/>
       
     <meta charset="UTF-8">
 
